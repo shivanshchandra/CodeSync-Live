c06afeb108e385cbe4d4ed5c6e6beb4d410b9e5e
Shivansh Chandra <shivansh.chandra96@gmail.com>
2026-02-19 20:53:34 +0530

    Rename the API URL for Deployment

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
 
 export const api = axios.create({
   baseURL: API_BASE,
